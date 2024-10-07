07b71d3f38c1c6cc70cdda98fa75d7d3366d5f24
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-10-07 18:20:56 +0300

    commit

--- src/components/atoms/Filters/FilterSection.tsx
+++ src/components/atoms/Filters/FilterSection.tsx
@@ -15,19 +15,27 @@ const FilterSection: React.FC<FilterSectionProps> = ({
   const [isOpen, setIsOpen] = useState(defaultOpen);
 
   return (
-    <div className="border-b border-gray-200 py-4 ">
+    <div className="border-b border-gray-200 py-4">
       <button
-        className="flex justify-between items-center  w-full text-left font-segoe text-lg"
+        className="flex justify-between items-center w-full text-left font-segoe text-lg"
         onClick={() => setIsOpen(!isOpen)}
       >
         {title}
         {isOpen ? (
-          <ChevronUp size={20} color="black" />
+          <ChevronUp size={20} color="green" />
         ) : (
-          <ChevronDown size={20} color="black" />
+          <ChevronDown size={20} color="green" />
         )}
       </button>
-      {isOpen && <div className="mt-2 ">{children}</div>}
+
+      {/* Add a container for the content with a transition */}
+      <div
+        className={`mt-2 transition-all duration-700 overflow-hidden ${
+          isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="select-none">{children}</div>
+      </div>
     </div>
   );
 };
